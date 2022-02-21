cfc5257ebb0e98574c6a9ced4011db5259bf6be4
Topkinsme <46650272+Topkinsme@users.noreply.github.com>
2022-02-21 20:41:52 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='enig',                         
+    packages=['enig'],                     
+    version='0.0.1',                               
+    license='GNU GPL v3'
+    description='Testing installation of Package',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Topkinsme',
+    url='https://github.com/Topkinsme/enigma_machine', 
+    install_requires=['random'],                  
+    keywords=["enigma","enigma_machine","topkinsme","enig"],
+    classifiers=[ 
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Documentation',
+        'License :: Other/Proprietary License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    
+    download_url="placeholder",
+)
